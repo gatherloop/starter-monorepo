b1112b4dfbc67a703ef04041d330857beaed6aaf
AndriPurnomo <andri.adrp@gmail.com>
2023-01-28 21:21:28 +0700

    chore: adding page heading organisms

---
+++ libs/ui/src/presentations/organisms/page-heading/PageHeading.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { PageHeading } from './PageHeading';
+
+export default {
+  component: PageHeading,
+} as ComponentMeta<typeof PageHeading>;
+
+const Template: ComponentStory<typeof PageHeading> = (args) => (
+  <PageHeading {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Edit Course',
+};
+
+export const HasBackIcon = Template.bind({});
+HasBackIcon.args = {
+  title: 'Edit Course',
+  hasBackIcon: true,
+  onBackIconPress: () => console.log(),
+};
