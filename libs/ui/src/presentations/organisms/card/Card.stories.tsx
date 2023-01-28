a4d7717e6c0be5c348ff5f5df26afff24aaeacfd
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2023-01-28 20:54:07 +0700

    feat: implement card organisms

---
+++ libs/ui/src/presentations/organisms/card/Card.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Card } from './Card';
+import { getAssetSrc } from '../../../utils';
+import { Button } from 'tamagui';
+
+export default {
+  component: Card,
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  avatarImageSrc: getAssetSrc('images/Avatar'),
+  items: [
+    { label: 'Name', value: 'M. Nindra Zaka' },
+    { label: 'Phone', value: '085573527382' },
+  ],
+};
