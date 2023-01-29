6c415e0213048a7fb3dac7e156854c7ccb32775a
milhamsuryapratama <ilham.pratama@ruangguru.com>
2023-01-29 11:00:36 +0700

    feat: GetContactList, GetContactByID and UpdateContactByID

--- apps/server/main.go
+++ apps/server/main.go
@@ -1,37 +1,40 @@
 package main
 
 import (
-  "course-explorer-monorepo/apps/server/config"
-  "course-explorer-monorepo/apps/server/config/database"
-  "course-explorer-monorepo/apps/server/core/module"
-  "course-explorer-monorepo/apps/server/core/repository"
-  "course-explorer-monorepo/apps/server/handler"
-  "fmt"
-  "github.com/gorilla/mux"
-  "github.com/joho/godotenv"
-  "log"
-  "net/http"
-  "os"
+	"course-explorer-monorepo/apps/server/config"
+	"course-explorer-monorepo/apps/server/config/database"
+	"course-explorer-monorepo/apps/server/core/module"
+	"course-explorer-monorepo/apps/server/core/repository"
+	"course-explorer-monorepo/apps/server/handler"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  // get config
-  cfg := config.Get()
+	// get config
+	cfg := config.Get()
 
-  // init database
-  db := database.Init(cfg)
+	// init database
+	db := database.Init(cfg)
 
-  contactRepo := repository.NewContactRepository(db)
-  contactUsecase := module.NewContactUsecase(contactRepo)
-  contactHandler := handler.NewContactHandler(contactUsecase)
+	contactRepo := repository.NewContactRepository(db)
+	contactUsecase := module.NewContactUsecase(contactRepo)
+	contactHandler := handler.NewContactHandler(contactUsecase)
 
-  mux := mux.NewRouter()
-  mux.HandleFunc("/contacts", contactHandler.CreateContact).Methods("POST")
+	mux := mux.NewRouter()
+	mux.HandleFunc("/contacts", contactHandler.CreateContact).Methods("POST")
+	mux.HandleFunc("/contacts", contactHandler.GetContactList).Methods("GET")
+	mux.HandleFunc("/contacts/{id}", contactHandler.GetContactByID).Methods("GET")
+	mux.HandleFunc("/contacts/{id}", contactHandler.UpdateContactByID).Methods("PUT")
 
-  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT")), mux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT")), mux))
 }
