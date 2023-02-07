c8ef099fbfc24436e02316463b1f5bb801cd314b
AndriPurnomo <andri.adrp@gmail.com>
2023-02-07 21:11:17 +0700

    fix: remove unused types

--- libs/ui/src/containers/widgets/contact-form-widget/ContactFormWidget.fetcher.tsx
+++ libs/ui/src/containers/widgets/contact-form-widget/ContactFormWidget.fetcher.tsx
@@ -2,11 +2,6 @@ import { contactApi } from 'libs/ui/src/domains';
 import { ContactUpdateScreenProps } from '../../screens';
 import { ContactFormWidgetProps } from './ContactFormWidget';
 
-export type GetContactForm = Extract<
-  ContactFormWidgetProps['variant'],
-  { type: 'update' }
->;
-
 export async function getContactFormWidgetInitialData(
   id: number
 ): Promise<ContactUpdateScreenProps['initialData']> {
