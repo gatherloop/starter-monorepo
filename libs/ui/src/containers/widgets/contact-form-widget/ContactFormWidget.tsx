45794d14177b0f12db4236f53e85c6317faa8626
AndriPurnomo <andri.adrp@gmail.com>
2023-02-10 19:08:51 +0700

    feat: add form state machine

--- libs/ui/src/containers/widgets/contact-form-widget/ContactFormWidget.tsx
+++ libs/ui/src/containers/widgets/contact-form-widget/ContactFormWidget.tsx
@@ -1,12 +1,13 @@
 import {
   useCreateContactMutation,
-  useGetContactByIdQuery,
   useUpdateContactMutation,
 } from 'libs/ui/src/machines';
 import { ErrorView, Form, FormPops, Skeleton } from 'libs/ui/src/presentations';
 import React from 'react';
-import { YStack } from 'tamagui';
+import { AlertDialog, Text, YStack } from 'tamagui';
+import { match } from 'ts-pattern';
 import { GetContactByID } from '../../../domains/';
+import { useContactWidgetMachine } from './ContactFormWidget.machine';
 
 type ContactFormWidgetVariant =
   | { type: 'create' }
@@ -24,26 +25,21 @@ export const ContactFormWidget = (props: ContactFormWidgetProps) => {
   const [name, setName] = React.useState('');
   const [phone, setPhone] = React.useState('');
   const [profilePictureURL, setProfilePictureUrl] = React.useState('');
-
-  const { status, data, refetch } = useGetContactByIdQuery({
-    initialData:
-      props.variant.type === 'update' ? props.variant.initialData : undefined,
-    id: props.variant.type === 'update' ? props.variant.id : 0,
-    enabled: props.variant.type === 'update',
-  });
-
-  const { mutate: mutateUpdate, isLoading: isLoadingUpdate } =
-    useUpdateContactMutation();
-  const { mutate: mutateCreate, isLoading: isLoadingCreate } =
-    useCreateContactMutation();
+  const [state, dispatch] = useContactWidgetMachine(
+    {
+      variant: props.variant,
+    },
+    props.variant.type === 'update' ? props.variant.initialData : undefined
+  );
 
   React.useEffect(() => {
-    if (data?.data) {
-      setName(data.data.name);
-      setPhone(data.data.phone);
-      setProfilePictureUrl(data.data.profilePictureURL);
+    if (state.type === 'formReady') {
+      setName(state.formValues.name);
+      setPhone(state.formValues.phone);
+      setProfilePictureUrl(state.formValues.profilePictureURL);
     }
-  }, [data?.data]);
+  }, [state.type]);
+
   const fields: FormPops['fields'] = [
     {
       label: 'Name',
@@ -68,72 +64,95 @@ export const ContactFormWidget = (props: ContactFormWidgetProps) => {
     },
   ];
 
-  const handleSubmit = () => {
-    props.variant.type === 'create'
-      ? mutateCreate({
-          payload: {
-            name,
-            phone,
-            profilePictureURL,
-          },
-        })
-      : mutateUpdate({
-          id: props.variant.id,
-          payload: {
-            name,
-            phone,
-            profilePictureURL,
-          },
-        });
-  };
-
   const renderView = () => {
-    switch (props.variant.type) {
-      case 'create':
-        return (
-          <Form
-            fields={fields}
-            onSubmit={handleSubmit}
-            isSubmitting={isLoadingCreate}
-          />
-        );
-      case 'update':
-        switch (status) {
-          case 'idle':
-          case 'loading': {
-            return (
-              <Skeleton isLoading>
-                <Form
-                  fields={fields}
-                  onSubmit={handleSubmit}
-                  isSubmitting={isLoadingUpdate}
-                />
-              </Skeleton>
-            );
-          }
-          case 'error': {
-            return (
-              <ErrorView
-                variant={{
-                  tag: 'fetching-error',
-                  onRetryButtonPress: refetch,
-                }}
-              />
-            );
-          }
-          case 'success': {
-            return (
+    return match(props.variant)
+      .with({ type: 'create' }, () =>
+        match(state)
+          .with({ type: 'formReady' }, (t) => (
+            <Form
+              fields={fields}
+              onSubmit={() => dispatch({ type: 'CREATE' })}
+              isSubmitting={false}
+            />
+          ))
+          .with({ type: 'creating' }, () => (
+            <Form fields={fields} onSubmit={() => null} isSubmitting={true} />
+          ))
+          .with({ type: 'submittingSuccess' }, () => (
+            <>
               <Form
                 fields={fields}
-                onSubmit={handleSubmit}
-                isSubmitting={isLoadingUpdate}
+                onSubmit={() => null}
+                isSubmitting={false}
               />
-            );
-          }
-          default:
-            break;
-        }
-    }
+              <AlertDialog>
+                <AlertDialog.Description>
+                  Successfully create data
+                </AlertDialog.Description>
+              </AlertDialog>
+            </>
+          ))
+          .with({ type: 'error' }, () => (
+            <ErrorView
+              variant={{
+                tag: 'fetching-error',
+                onRetryButtonPress: () => null,
+              }}
+            />
+          ))
+          .otherwise(() => null)
+      )
+      .with({ type: 'update' }, () =>
+        match(state)
+          .with({ type: 'idle' }, { type: 'loading' }, () => (
+            <Skeleton isLoading>
+              <Form
+                fields={fields}
+                onSubmit={() => null}
+                isSubmitting={false}
+              />
+            </Skeleton>
+          ))
+          .with({ type: 'formReady' }, () => (
+            <Form
+              fields={fields}
+              onSubmit={() =>
+                dispatch({
+                  type: 'UPDATE',
+                  id: props.variant.type === 'update' ? props.variant.id : 0,
+                })
+              }
+              isSubmitting={false}
+            />
+          ))
+          .with({ type: 'updating' }, () => (
+            <Form fields={fields} onSubmit={() => null} isSubmitting={true} />
+          ))
+          .with({ type: 'submittingSuccess' }, () => (
+            <>
+              <Form
+                fields={fields}
+                onSubmit={() => null}
+                isSubmitting={false}
+              />
+              <AlertDialog>
+                <AlertDialog.Description>
+                  Successfully update data
+                </AlertDialog.Description>
+              </AlertDialog>
+            </>
+          ))
+          .with({ type: 'error' }, () => (
+            <ErrorView
+              variant={{
+                tag: 'fetching-error',
+                onRetryButtonPress: () => null,
+              }}
+            />
+          ))
+          .otherwise(() => null)
+      )
+      .otherwise(() => null);
   };
 
   return (
